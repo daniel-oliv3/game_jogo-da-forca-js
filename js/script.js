d08c9c7f3667067ddeb84a9ab673f9e573a64e8c
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-09-01 16:27:40 -0300

    Update

---
+++ js/script.js
@@ -0,0 +1,148 @@
+/* ======= Lista de palavras(Objstos) ======= */
+const palavras = [
+    palavra001={
+        nome = "IRLANDA",
+        categoria = "LUGARES"
+    },
+    palavra002={
+        nome = "EQUADOR",
+        categoria = "LUGARES"
+    },
+    palavra003={
+        nome = "INDONESIA",
+        categoria = "LUGARES"
+    },
+    palavra004={
+        nome = "MALDIVAS",
+        categoria = "LUGARES"
+    },
+    palavra005={
+        nome = "INGLATERRA",
+        categoria = "LUGARES"
+    },
+    palavra006={
+        nome = "UZBEQUISTAO",
+        categoria = "LUGARES"
+    },
+    palavra007={
+        nome = "CHINA",
+        categoria = "LUGARES"
+    },
+    palavra008={
+        nome = "PORTUGAL",
+        categoria = "LUGARES"
+    },
+    palavra009={
+        nome = "AMAZONAS",
+        categoria = "LUGARES"
+    },
+    palavra010={
+        nome = "MANAUS",
+        categoria = "LUGARES"
+    },
+    palavra011={
+        nome = "BICICLETA",
+        categoria = "TRANSPORTE"
+    },
+    palavra012={
+        nome = "LANCHA",
+        categoria = "TRANSPORTE"
+    },
+    palavra013={
+        nome = "NAVIO",
+        categoria = "TRANSPORTE"
+    },
+    palavra014={
+        nome = "TELEFERICO",
+        categoria = "TRANSPORTE"
+    },
+    palavra015={
+        nome = "MOTOCICLETA",
+        categoria = "TRANSPORTE"
+    },
+    palavra016={
+        nome = "BARCO",
+        categoria = "TRANSPORTE"
+    },
+    palavra017={
+        nome = "AERONAVE",
+        categoria = "TRANSPORTE"
+    },
+    palavra018={
+        nome = "SKATEBOARDING",
+        categoria = "TRANSPORTE"
+    },
+    palavra019={
+        nome = "ONIBUS",
+        categoria = "TRANSPORTE"
+    },
+    palavra020={
+        nome = "IATE",
+        categoria = "TRANSPORTE"
+    },
+    palavra011={
+        nome = "BICICLETA",
+        categoria = "TRANSPORTE"
+    },
+    palavra012={
+        nome = "LANCHA",
+        categoria = "TRANSPORTE"
+    },
+    palavra013={
+        nome = "NAVIO",
+        categoria = "TRANSPORTE"
+    },
+    palavra014={
+        nome = "TELEFERICO",
+        categoria = "TRANSPORTE"
+    },
+    palavra015={
+        nome = "MOTOCICLETA",
+        categoria = "TRANSPORTE"
+    },
+    palavra016={
+        nome = "BARCO",
+        categoria = "TRANSPORTE"
+    },
+    palavra017={
+        nome = "AERONAVE",
+        categoria = "TRANSPORTE"
+    },
+    palavra018={
+        nome = "SKATEBOARDING",
+        categoria = "TRANSPORTE"
+    },
+    palavra019={
+        nome = "ONIBUS",
+        categoria = "TRANSPORTE"
+    },
+    palavra020={
+        nome = "IATE",
+        categoria = "TRANSPORTE"
+    },
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ======= / ======= */
+
+
+
+
+
